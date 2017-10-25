bfe48f3683ee8a9345a56e765241a6a8a1e74e88
Paul Huynh <paulhuynh94@gmail.com>
2017-10-25 11:58:31 -0700

    Added Navigation
    Made a temp second page to create tab navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import { TabNavigator } from "react-navigation";
 
-export default class App extends React.Component {
+
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Home page'
+  };
   render() {
+    //const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
         <Text>Welcome to Shwop!</Text>
         <Image source={require('./assets/shwop-portland-or.jpg')} style={{width: 333, height: 130}}/>
+
       </View>
     );
+  }
+}
+
+class SecondScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Second Screen',
+  };
+  render() {
+    return(
+      <View>
+        <Text>Yo Yo Yo</Text>
+      </View>
+    );
+  }
+}
+
+const ShwopApp = TabNavigator({
+    Home: {
+      screen: HomeScreen
+    },
+    Second: { screen: SecondScreen}
+});
+
+export default class App extends React.Component {
+  render() {
+    return <ShwopApp />;/*(
+      <View style={styles.container}>
+        <Text>Welcome to Shwop!</Text>
+        <Image source={require('./assets/shwop-portland-or.jpg')} style={{width: 333, height: 130}}/>
+      </View>
+    );*/
   }
 }
 
