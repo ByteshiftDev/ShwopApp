dcfc6333db2c6cb80ca6ae757f52ede3ff00d491
Cher Moua <mouacher@pdx.edu>
2017-11-08 13:02:02 -0800

    Seperate files
    Broke the views down into their own file component.

---
+++ src/Style.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  icon:{
+    width: 24,
+    height: 24
+  },
+});
+
+export default styles;
