de53323659066ce8afe8fcac964cc68a0a695db4
Paul Huynh <paulhuynh94@gmail.com>
2017-11-10 12:11:10 -0800

    Added SignIn and sSignUp files

---
+++ src/SignIn.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+import { View, Text, TouchableOpacity, TextInput, Image, StyleSheet } from 'react-native'
+
+class SignIn extends Component {
+   state = {
+      email: '',
+      password: ''
+   }
+   handleEmail = (text) => {
+      this.setState({ email: text })
+   }
+   handlePassword = (text) => {
+      this.setState({ password: text })
+   }
+   login = (email, pass) => {
+      alert('email: ' + email + ' password: ' + pass)
+   }
+   render(){
+      return (
+         <View style = {styles.container}>
+          <Image source={require('./assets/shwop-portland-or.jpg')} style={{width: 333, height: 130}}/>
+          <TextInput style = {styles.input}
+               underlineColorAndroid = "transparent"
+               placeholder = "Email"
+               placeholderTextColor = "#9a73ef"
+               autoCapitalize = "none"
+               onChangeText = {this.handleEmail}/>
+
+            <TextInput style = {styles.input}
+               underlineColorAndroid = "transparent"
+               placeholder = "Password"
+               placeholderTextColor = "#9a73ef"
+               autoCapitalize = "none"
+               onChangeText = {this.handlePassword}/>
+
+            <TouchableOpacity
+               style = {styles.submitButton}
+               onPress = {
+                  () => this.login(this.state.email, this.state.password)
+               }>
+               <Text style = {styles.submitButtonText}> Sign In </Text>
+            </TouchableOpacity>
+         </View>
+      )
+   }
+}
+
+export default SignIn
+
+const styles = StyleSheet.create({
+   container: {
+      paddingTop: 23,
+      flex: 1,
+   },
+   input: {
+      margin: 15,
+      height: 40,
+      borderColor: '#7a42f4',
+      borderWidth: 1
+   },
+   submitButton: {
+      backgroundColor: '#7a42f4',
+      padding: 10,
+      margin: 15,
+      height: 40,
+   },
+   submitButtonText:{
+      color: 'white'
+   }
+})
