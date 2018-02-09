688d9f97963dc74d7936aff292b286504f8b75a3
Paul Huynh <paulhuynh94@gmail.com>
2018-02-08 21:12:52 -0800

    Load data for women catagory

--- src/Categories.js
+++ src/Categories.js
@@ -1,13 +1,49 @@
 import styles from './Style.js';
 import React from 'react';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { StyleSheet, Text, View, Image, FlatList, Button } from 'react-native';
 import { TabNavigator } from "react-navigation";
 
 class Women extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true
+    }
+  }
+
+  componentDidMount() {
+    console.log("Making API call...");
+    return fetch('https://shwop-api.herokuapp.com/item/filter?query=womens')
+    .then((response) => response.json())
+    .then((responseJson) => { this.setState({
+        isLoading: false,
+        dataSource: responseJson,
+      });
+      console.log("Finished API call... Done!")
+      return responseJson;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
+
   render(){
+    console.log("Attempting to load files from datasource...");
+    console.log(this.state.dataSource);
     return(
-      <View>
-        <Text>Women</Text>
+      <View style={styles.container}>
+        <FlatList
+          numColumns={2}
+          data={this.state.dataSource}
+          renderItem={({item}) =>
+          <View style={styles.gridItem}>
+            <View style={{alignItems:'center'}}>
+              <Image source={{ uri: item.url }} style={styles.imageTile} />
+            </View>
+            <Text style={styles.imageTileText}>{item.name}</Text>
+          </View>
+        }
+        />
       </View>
     );
   }
